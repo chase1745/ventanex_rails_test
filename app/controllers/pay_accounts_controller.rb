f5e7397a8474021901e6aa56ec2ddb1e6200c28c
Chase McDermott <chasemcd1745@tamu.edu>
2019-08-26 20:47:29 -0500

    Add actions for show and delete buttons.

--- app/controllers/pay_accounts_controller.rb
+++ app/controllers/pay_accounts_controller.rb
@@ -1,4 +1,5 @@
 class PayAccountsController < ApplicationController
+  before_action :get_pay_account, only: [:show, :destroy, :new]
 
   def index
     @pay_accounts = PayAccount.all
@@ -11,6 +12,21 @@ class PayAccountsController < ApplicationController
   end
 
   def destroy
+    if @pay_account.destroy
+      redirect_to pay_accounts_path
+    else
+      redirect_to pay_account_path(@pay_account) # TODO: addition would be adding errors showing user why it failed.
+    end
+  end
+
+  private
+
+  def pay_account_params
+    params.require(:pay_account).permit(:nickname, :account_type, :account_number)
+  end
+
+  def get_pay_account
+    @pay_account = PayAccount.find(params[:id])
   end
 
 end
